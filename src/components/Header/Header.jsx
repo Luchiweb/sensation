842cd7850fb7eae791f93eb1ccb6f22c1ee56d5e
Svetlana <svetlana.moiseienko.web@gmail.com>
2024-04-12 15:23:31 +0400

    feat: adaptive layout for header and hero blocks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,21 +3,31 @@ import './Header.css';
 import logo from "../../assets/icons/logo.svg"
 import dendalion from "../../assets/icons/logo-dendalion.svg"
 import cart from "../../assets/icons/cart.svg"
+import { useState } from 'react';
 
 function Header() {
+
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
     return (
         <div className="header container">
+            <button className="burger-menu" aria-label="Toggle menu" onClick={toggleNav}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
             <div className="logo">
                 <img className="logo-text" src={logo} alt="logo" />
                 <img className="logo-dendalion" src={dendalion} alt="dendalion" />
             </div>
-            <nav className="navbar">
+            <nav className={`navbar ${isNavVisible ? 'visible' : ''}`}>
                 <a href="#">About us</a>
                 <a href="#">Photo Books</a>
                 <a href="#">Prints & Frames</a>
                 <a href="#">Wall art</a>
-                <a href="#">Gifts</a>
-                <a href="#">Orders</a>
             </nav>
             <div><img src={cart} alt="cart" /></div>
         </div>
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,21 +1,21 @@
-import BookCollection from '../../components/BookCollection/BookCollection';
-import ExtraService from '../../components/ExtraService/ExtraService';
+// import BookCollection from '../../components/BookCollection/BookCollection';
+// import ExtraService from '../../components/ExtraService/ExtraService';
 import Header from '../../components/Header/Header';
 import Hero from '../../components/Hero/Hero';
-import HowItWorks from '../../components/HowItWorks/HowItWorks';
-import Promo from '../../components/Promo/Promo';
-import Reviews from '../../components/Reviews/Reviews';
+// import HowItWorks from '../../components/HowItWorks/HowItWorks';
+// import Promo from '../../components/Promo/Promo';
+// import Reviews from '../../components/Reviews/Reviews';
 
 function HomePage() {
   return (
     <>
       <Header />
       <Hero />
-      <BookCollection />
+      {/* <BookCollection />
       <HowItWorks />
       <ExtraService />
       <Reviews />
-      <Promo />
+      <Promo /> */}
     </>
   );
 }
