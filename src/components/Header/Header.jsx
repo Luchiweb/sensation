1d9d82e76bf588e30e36b0f5ca1e62eac2241169
Svetlana <svetlana.moiseienko.web@gmail.com>
2024-04-29 09:40:38 +0400

    feat: add burger menu animation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,38 @@
 import './Header.css';
 
-import logo from "../../assets/icons/logo.svg"
-import dendalion from "../../assets/icons/logo-dendalion.svg"
-import cart from "../../assets/icons/cart.svg"
+import logo from '../../assets/icons/logo.svg';
+import dendalion from '../../assets/icons/logo-dendalion.svg';
+import cart from '../../assets/icons/cart.svg';
 import { useState } from 'react';
 
 function Header() {
+  const [isNavVisible, setIsNavVisible] = useState(false);
 
-    const [isNavVisible, setIsNavVisible] = useState(false);
-
-    const toggleNav = () => {
-        setIsNavVisible(!isNavVisible);
-    };
-    return (
-        <div className="header container">
-            <button className="burger-menu" aria-label="Toggle menu" onClick={toggleNav}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-            <div className="logo">
-                <img className="logo-text" src={logo} alt="logo" />
-                <img className="logo-dendalion" src={dendalion} alt="dendalion" />
-            </div>
-            <nav className={`navbar ${isNavVisible ? 'visible' : ''}`}>
-                <a href="#">About us</a>
-                <a href="#">Photo Books</a>
-                <a href="#">Prints & Frames</a>
-                <a href="#">Wall art</a>
-            </nav>
-            <div><img src={cart} alt="cart" /></div>
-        </div>
-    )
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+  return (
+    <div className={`header container ${isNavVisible ? 'nav-visible' : ''}`}>
+      <button className="burger-menu" aria-label="Toggle menu" onClick={toggleNav}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <a href="#" className="logo">
+        <img className="logo-text" src={logo} alt="logo" />
+        <img className="logo-dendalion" src={dendalion} alt="dendalion" />
+      </a>
+      <nav className="navbar">
+        <a href="#">About us</a>
+        <a href="#">Photo Books</a>
+        <a href="#">Prints & Frames</a>
+        <a href="#">Wall art</a>
+      </nav>
+      <a href="#" className="cart">
+        <img src={cart} alt="cart" />
+      </a>
+      <div className="backdrop" onClick={toggleNav}></div>
+    </div>
+  );
 }
-export default Header
+export default Header;
